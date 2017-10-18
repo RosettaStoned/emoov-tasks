7d0703628c7750fe86bd96e36c3d173a394f0d4e
RosettaStoned <dimitar.kod@gmail.com>
2017-10-18 17:26:09 +0300

    make an obj and call a method to generate book

--- book_generator.py
+++ book_generator.py
@@ -1,7 +1,8 @@
-#!/usr/bin/env python3                                                          
-                                                                                
-""" A generator for random books"""                                             
-from src.book import BookGenerator                                             
-                                                                                
-if __name__ == '__main__':                                                      
-    BOOKS = BookGenerator(10, (10, 33)).generate()
+#!/usr/bin/env python3
+
+""" A generator for random books"""
+from src.book import BookGenerator
+
+if __name__ == '__main__':
+    bg = BookGenerator(10, (10, 33))
+    bg.generate()
